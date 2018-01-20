3e64c998c0a94ecb66589cded8cd114c7bbe976e
MaboroshiChan <31759226+MaboroshiChan@users.noreply.github.com>
2018-01-20 05:53:04 -0800

    Update 测试.py

--- 测试.py
+++ 测试.py
@@ -1,3 +1,28 @@
 import BDTB
 import Count
-import 
+import Tool
+import urllib.request
+import re
+
+datas =[]
+for i in range(1,19):
+
+    BaseUrl = 'https://tieba.baidu.com/p/5475081737'
+    bdtb = BDTB(BaseUrl, 0)
+    G = bdtb.getPage(i)
+    temp = bdtb.getContent(G)
+    datas += list(temp)
+F = open('Contests.txt', encoding='utf-8')
+A = F.read()
+
+
+Example = Count(raw=A, data=datas)
+
+Cont = Count.CountCont(self=Example)
+vote = Count.getCont(self=Example,Cont=Cont)
+vote = Count.start(self=Example,vote=vote)
+result = Count.result(self=Example,Cont=Cont, vote=vote)
+
+
+print(result)
+
